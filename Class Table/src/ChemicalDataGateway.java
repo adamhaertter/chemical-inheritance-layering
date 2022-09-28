b062380f204d9f266848b96aa10fdce97dcf2456
Adam Haertter <ah2005@engr.ship.edu>
2022-09-28 16:44:20 -0400

    Add Gateway and basic structure for ChemicalDataGateway

---
+++ Class Table/src/ChemicalDataGateway.java
@@ -0,0 +1,17 @@
+public class ChemicalDataGateway extends Gateway {
+    
+    private long id = 0;
+    private String name = "";
+
+    
+    public ChemicalDataGateway(long id) {
+        id = this.id;
+        deleted = false;
+    }
+
+    public ChemicalDataGateway(String name) {
+        // Since we are removing inhabits, we don't set that up here
+        name = this.name;
+        deleted = false;
+    }
+}
