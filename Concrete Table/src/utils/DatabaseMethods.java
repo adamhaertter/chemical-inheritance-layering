fb2874f00b2e36a60f0aa18bc49727a9d03b992d
Scott Bucher <stqlth@gmail.com>
2022-09-29 18:08:19 -0400

    Reworked config

--- Concrete Table/src/utils/DatabaseMethods.java
+++ Concrete Table/src/utils/DatabaseMethods.java
@@ -1,12 +1,12 @@
-package src.utils;
+package utils;
 
-import net.dv8tion.jda.api.entities.*;
+import config.ProjectConfig;
 
 import java.sql.*;
 
 public class DatabaseMethods {
     public void addBase(String name, long solute) throws SQLException {
-        try (Connection conn = DriverManager.getConnection(BirthdayBotConfig.getDbUrl(), BirthdayBotConfig.getDbUser(), BirthdayBotConfig.getDbPassword());
+        try (Connection conn = DriverManager.getConnection(ProjectConfig.DatabaseURL, ProjectConfig.DatabaseUser, ProjectConfig.DatabasePassword);
              CallableStatement statement = conn.prepareCall("CALL Base_Add(?, ?)")) {
 
             statement.setString(1, name);
