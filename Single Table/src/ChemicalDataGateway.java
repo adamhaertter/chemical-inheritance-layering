977613fa060141d1c979cd13b5889402fe45dd64
Brennan Mulligan <bcmulligan7@gmail.com>
2022-10-04 15:33:02 -0400

    Addition of JavaDoc comments and constructrs for Chemical and Compound

--- Single Table/src/ChemicalDataGateway.java
+++ Single Table/src/ChemicalDataGateway.java
@@ -7,119 +7,253 @@ public class ChemicalDataGateway extends Gateway {
     double atomicMass = 0.0;
     int baseSolute = 0;
     int acidSolute = 0;
-    String[] dissolved;
+    String[] dissolves;
     long dissolvedBy = 0;
+    String type = "";
 
+    /**
+     * Constructor that uses the id to create a row data gateway
+     *
+     * @param identification - the id for each chemical
+     * @throws SQLException
+     */
     public ChemicalDataGateway(long identification) throws SQLException {
-        id = identification;
+        super();
+        this.id = identification;
+        try {
+            CallableStatement statement = conn.prepareCall("SELECT * FROM 'Chemical' WHERE id = '" + id + "'");
+            ResultSet rs = statement.executeQuery();
+            this.name = rs.getString("name");
+            this.atomicNumber = rs.getInt("atomicNumber");
+            this.atomicMass = rs.getInt("atomicMass");
+            this.baseSolute = rs.getInt("baseSolute");
+            this.acidSolute = rs.getInt("acidSolute");
+            this.dissolvedBy = rs.getLong("dissolvedBy");
+            this.type = rs.getString("type");
+
+            statement = conn.prepareCall("SELECT dissolvedBy FROM 'Chemical'" +
+                                                "WHERE id = '" + id + "'");
+            rs = statement.executeQuery();
+            for(int i = 0; rs.next(); i++) {
+                dissolves[i] = rs.getString(i);
+            }
+            persist();
+        } catch(Exception ex) {
+            // Some other error (There is not an error if the entry doesn't exist)
+        }
     }
 
+    /**
+     * Constructor used to assign values to a list of variables
+     * @param n - the name of the chemical
+     * @param number - the atomic number of the chemical
+     * @param mass - the atomic mass of the chemical
+     * @param bSolute - the base solute of the chemical
+     * @param aSolute - the acid solute of the chemical
+     * @param diss - the list of chemicals that are capable of being dissolved
+     * @param dissBy - the acid that a chemical is dissolved by
+     * @param type -
+     */
     public ChemicalDataGateway(String n, int number, double mass, int bSolute,
-                               int aSolute, String[] diss, long dissBy) {
-        name = n;
-        atomicNumber = number;
-        atomicMass = mass;
-        baseSolute = bSolute;
-        acidSolute = aSolute;
-        dissolved = diss;
-        dissolvedBy = dissBy;
+                               int aSolute, String[] diss, long dissBy, String type) {
+        super();
+        this.name = n;
+        this.atomicNumber = number;
+        this.atomicMass = mass;
+        this.baseSolute = bSolute;
+        this.acidSolute = aSolute;
+        this.dissolves = diss;
+        this.dissolvedBy = dissBy;
+        this.type = type;
+
+        
+
+        persist();
     }
 
     //Getters & Setters ----------------------------------------------------------------------
+    /**
+     * Getter for the id
+     * @return id - the identification
+     */
     public long getID() {
         verifyExistence();
         return id;
     }
 
+    /**
+     * Getter for the name
+     * @return name - the name of the chemical
+     */
     public String getName() {
         verifyExistence();
         return name;
     }
 
+    /**
+     * Setter for the name
+     * @param name - the name of the chemical
+     */
     public void setName(String name) {
         verifyExistence();
         this.name = name;
         persist();
     }
 
+    /**
+     * Getter for the atomic number
+     * @return atomicNumber - the atomic number of the chemical
+     */
     public int getAtomicNumber() {
         verifyExistence();
         return atomicNumber;
     }
 
+    /**
+     * Setter for the atomic number
+     * @param atomicNumber - the atomic number of the chemical
+     */
     public void setAtomicNumber(int atomicNumber) {
         verifyExistence();
         this.atomicNumber = atomicNumber;
         persist();
     }
 
+    /**
+     * Getter for the atomic mass
+     * @return atomicMass - the atomic mass of the chemical
+     */
     public double getAtomicMass() {
         verifyExistence();
         return atomicMass;
     }
 
+    /**
+     * Setter for the atomic mass
+     * @param atomicMass - the atomic mass of the chemical
+     */
     public void setAtomicMass(double atomicMass) {
         verifyExistence();
         this.atomicMass = atomicMass;
         persist();
     }
 
+    /**
+     * Getter for the base solute
+     * @return baseSolute - the base solute of the chemical
+     */
     public int getBaseSolute() {
         verifyExistence();
         return baseSolute;
     }
 
+    /**
+     * Setter for the base solute
+     * @param baseSolute - the base solute of the chemical
+     */
     public void setBaseSolute(int baseSolute) {
         verifyExistence();
         this.baseSolute = baseSolute;
         persist();
     }
 
+    /**
+     * Getter for the acid solute
+     * @return acidSolute - the acid solute of the chemical
+     */
     public int getAcidSolute() {
         verifyExistence();
         return acidSolute;
     }
 
+    /**
+     * Set acid solute
+     * @param acidSolute - the acid solute of the chemical
+     */
     public void setAcidSolute(int acidSolute) {
         verifyExistence();
         this.acidSolute = acidSolute;
         persist();
     }
 
-    public String[] getDissolved() {
+    /**
+     * Getter for dissolves
+     * @return dissolves - the list of chemicals that are capable of being dissolved
+     */
+    public String[] getDissolves() {
         verifyExistence();
-        return dissolved;
+        return dissolves;
     }
 
-    public void setDissolved(String[] dissolved) {
+    /**
+     * Setter for dissolves
+     * @param dissolves - the list of chemicals that are capable of being dissolved
+     */
+    public void setDissolves(String[] dissolves) {
         verifyExistence();
-        this.dissolved = dissolved;
+        this.dissolves = dissolves;
         persist();
     }
 
+    /**
+     * Getter for dissolvedBy
+     * @return dissolvedBy - the acid that a chemical is dissolved by
+     */
     public long getDissolvedBy() {
         verifyExistence();
         return dissolvedBy;
     }
 
+    /**
+     * Setter for dissolvedBy
+     * @param dissolvedBy - the acid that a chemical is dissolved by
+     */
     public void setDissolvedBy(long dissolvedBy) {
         verifyExistence();
         this.dissolvedBy = dissolvedBy;
         persist();
     }
 
+    /**
+     * Getter for dissolvedBy
+     * @return type - the acid that a chemical is dissolved by
+     */
+    public String getType() {
+        verifyExistence();
+        return type;
+    }
+
+    /**
+     * Setter for type
+     * @param type - the acid that a chemical is dissolved by
+     */
+    public void setType(String type) {
+        verifyExistence();
+        this.type = type;
+        persist();
+    }
+
+    /**
+     *
+     * @return true or false
+     */
     public boolean persist() {
         try {
             Statement statement = conn.createStatement();
-            statement.executeUpdate("UPDATE Chemical SET id = '" + id + "', name = '" + name +
-                    "' WHERE id = '" + id + "'");
+            statement.executeUpdate("UPDATE Chemical SET id = '" + id + "', name = '" + name + "', " +
+                    "atomicNumber = '" + atomicNumber + "', atomicMass = '" + atomicMass + "', baseSolute = '"
+                    + baseSolute + "', acidSolute = '" + acidSolute + "', dissolves = '" + dissolves +
+                    "', dissolvedBy = '" + dissolvedBy + "', type = '" + type + "' WHERE id = '" + id + "'");
         } catch (Exception ex) {
-            // Fails because already exists?
+            // Fails because already exists
             return false;
         }
         return true;
     }
 
+    /**
+     *
+     */
     private void verifyExistence() {
         if (deleted)
             try {
--- Single Table/src/CompoundDataGateway.java
+++ Single Table/src/CompoundDataGateway.java
@@ -7,31 +7,73 @@ public class CompoundDataGateway extends Gateway {
     long elementID;
     protected static Connection m_dbConn = null;
 
+    /**
+     *
+     * @param compound - the compoundID
+     */
     public CompoundDataGateway(long compound) {
+        super();
         this.compoundID = compound;
+        try {
+            CallableStatement statement = conn.prepareCall("SELECT * FROM 'CompoundToElement'" +
+                                                                "WHERE CompoundId = '" + CompoundID + "'");
+            ResultSet rs = statement.executeQuery();
+            this.elementID = rs.getLong("ElementId");
+            persist();
+        } catch(Exception ex) {
+            // Some other error (There is not an error if the entry doesn't exist)
+        }
     }
 
-    public CompoundDataGateway(long compound, long element) {
+    /**
+     *
+     * @param compound - the compoundID
+     * @param element - the elementID
+     */
+    public CompoundDataGatdeway(long compound, long element) {
+        super();
         this.compoundID = compound;
         this.elementID = element;
+        persist();
     }
 
+    /**
+     * Getter for compoundID
+     * @return compoundID - the identification of the compound
+     */
     public long getCompoundID() {
         return compoundID;
     }
 
+    /**
+     * Setter for compoundID
+     * @param compoundID - the identification of the compound
+     */
     public void setCompoundID(long compoundID) {
         this.compoundID = compoundID;
     }
 
+    /**
+     * Getter for elementID
+     * @return elementID - the identification of the element
+     */
     public long getElementID() {
         return elementID;
     }
 
+    /**
+     * Setter for elementID
+     * @param elementID - the identification of the element
+     */
     public void setElementID(long elementID) {
         this.elementID = elementID;
     }
 
+    /**
+     * Getter for all the compounds in the table
+     * @return allCompoundsList - a list of all the compounds in the table
+     * @throws SQLException
+     */
     public ArrayList<CompoundDataGateway> getAllCompounds() throws SQLException {
         ArrayList<CompoundDataGateway> allCompoundsList = new ArrayList<>();
         Statement statement = m_dbConn.createStatement();
@@ -41,6 +83,12 @@ public class CompoundDataGateway extends Gateway {
         return allCompoundsList;
     }
 
+    /**
+     * Getter f
+     * @param elemID - the identification of the element
+     * @return compoundList - a list of compounds
+     * @throws SQLException
+     */
     public ArrayList<CompoundDataGateway> getCompoundsByElementID(long elemID) throws SQLException {
         ArrayList<CompoundDataGateway> compoundList = new ArrayList<>();
         Statement statement = m_dbConn.createStatement();
@@ -57,6 +105,12 @@ public class CompoundDataGateway extends Gateway {
         return compoundList;
     }
 
+    /**
+     *
+     * @param compID - the identification of the compound
+     * @return compoundList - a list of compounds
+     * @throws SQLException
+     */
     public ArrayList<CompoundDataGateway> getCompoundElements(int compID) throws SQLException {
         ArrayList<CompoundDataGateway> compoundList = new ArrayList<>();
         Statement statement = m_dbConn.createStatement();
@@ -73,15 +127,25 @@ public class CompoundDataGateway extends Gateway {
         return compoundList;
     }
 
+    /**
+     *
+     * @return true or false
+     */
     public boolean persist() {
-        verifyExistence();
         try {
-        } catch(Error e) {
-
+            Statement statement = conn.createStatement();
+            statement.executeUpdate("UPDATE CompoundToElement SET CompoundId = '" + compoundID + "'," +
+                    "ElementId = '" + elementID + "' WHERE CompoundId = '" + compoundID + "'");
+        } catch (Exception ex) {
+            // Fails because already exists
+            return false;
         }
-
+        return true;
     }
 
+    /**
+     *
+     */
     private void verifyExistence() {
         if (deleted)
             try {
