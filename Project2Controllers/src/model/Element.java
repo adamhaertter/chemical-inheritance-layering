7a7dd60cf44803eab76ed8d0a9330a122c590fbe
Adam Haertter <ah2005@engr.ship.edu>
2022-10-17 16:21:01 -0400

    Initial Project2Controllers/ Commit from Dr. Wellington

---
+++ Project2Controllers/src/model/Element.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Element
+{
+    public String getName()
+    {
+        return null;
+    }
+
+    public int getAtomicNumber()
+    {
+        return 0;
+    }
+
+    public double getAtomicWeight()
+    {
+        return 0;
+    }
+}
---
+++ Project2Controllers/src/model/controller/ElementController.java
@@ -0,0 +1,21 @@
+package model.controller;
+
+import model.Element;
+
+public class ElementController
+{
+    public Element getMyElement()
+    {
+        return myElement;
+    }
+
+    private Element myElement;
+    public ElementController(String name)
+    {
+    }
+
+    public ElementController(String name, int atomicNumber, double atomicWeight)
+    {
+
+    }
+}
