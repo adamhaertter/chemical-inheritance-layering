7f935dd9aad2269bf3a7387017a524a2aede1083
Stqlth <stqlth@gmail.com>
2022-10-19 15:45:40 -0400

    Moved controllers to domain module

---
+++ Domain/src/model/ElementMapper.java
@@ -0,0 +1,26 @@
+package model;
+
+public class ElementMapper
+{
+    private Element myElement;
+    /**
+     * Create a new element in the database, and store the resulting model object
+     * into my instance variable
+     */
+    public ElementMapper(String name, int atomicNumber, double atomicWeight)
+    {
+    }
+
+    /**
+     * Constructor for objects that exist in the db
+     * @param name
+     */
+    public ElementMapper(String name)
+    {
+    }
+
+    public Element getMyElement()
+    {
+        return myElement;
+    }
+}
