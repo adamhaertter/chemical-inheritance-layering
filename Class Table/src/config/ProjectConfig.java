86f3c4e7f21067870667a85127088b048e8de263
Adam Haertter <ah2005@engr.ship.edu>
2022-11-27 03:54:24 -0500

    Move videos to Git LFS

---
+++ Class Table/src/config/ProjectConfig.java
@@ -0,0 +1,7 @@
+package config;
+
+public class ProjectConfig {
+    public static String DatabaseURL = "";
+    public static String DatabaseUser = "";
+    public static String DatabasePassword = "";
+}
